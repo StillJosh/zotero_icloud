901294e8c71cb588d3e1f9dbd0d46468b4b0f3d8
Joshua Stiller <joshua.stiller@tum.de>
2024-01-28 18:31:16 +0100

    ENH: Added functionality that listens to new items being added to the library, copies the pdfs to the icloud folder, attaches the pdf, and deletes the existing version in the zotero folder

---
+++ src/icloud-attacher.js
@@ -0,0 +1,109 @@
+ICloudAttacher = {
+	id: null,
+	version: null,
+	rootURI: null,
+	initialized: false,
+	addedElementIDs: [],
+	
+	init({ id, version, rootURI }) {
+		if (this.initialized) return;
+		this.id = id;
+		this.version = version;
+		this.rootURI = rootURI;
+		this.initialized = true;
+
+		let notifierID = Zotero.Notifier.registerObserver(this.newItemCallback, ['item']);
+
+		// Unregister callback when the window closes (important to avoid a memory leak)
+		window.addEventListener('unload', function(e) {
+			Zotero.Notifier.unregisterObserver(notifierID);
+		}, false);
+	},
+
+	newItemCallback: {
+		notify: function(event, type, ids) {
+
+			// Import the OS.File module
+			Components.utils.import("resource://gre/modules/osfile.jsm");
+			Zotero.debug("Item added: " + event + ' ' + type + ' ' + ids);
+
+			// Only act on item additions
+			if (event !== 'add' || type !== 'item') return;
+
+			Zotero.Items.get(ids).forEach(item => {
+				Zotero.debug("Item Invest: " + item.itemType + ' ' + item.attachmentContentType + ' ' + item.attachmentLinkMode);
+
+				// Only act on PDF attachments which are not linked
+				if (item.itemType !== 'attachment' || item.attachmentContentType !== 'application/pdf' || item.attachmentLinkMode !== 1) return;
+				Zotero.debug("Item checked: " + item);
+
+				// Copy the file to the iCloud folder
+				const iCloudPath = Zotero.Prefs.get('extensions.icloud-attacher.iCloudPath', true);
+				const targetFile = OS.Path.join(iCloudPath, item.attachmentFilename);
+
+				OS.File.copy(item.getFilePath(), targetFile, {noOverwrite: true})
+					.then(() => {
+						// Link the file to the item
+						Zotero.debug("The file has been copied to iCloud folder.");
+						const parentItem = Zotero.Items.get(item.parentID);
+						Zotero.debug("Target File: "  + targetFile);
+
+						Zotero.Attachments.linkFromFile({
+							file: targetFile,
+							libraryID: parentItem.libraryID,
+							parentItemID: parentItem.getID(),
+						});
+
+						// Delete the original file
+						item.eraseTx();
+					})
+					.catch(error => {
+							// If the file already exists, link it to the item
+							Zotero.debug("File already exists.");
+							Zotero.debug("Target File: "  + targetFile);
+
+						const parentItem = Zotero.Items.get(item.parentID);
+							Zotero.Attachments.linkFromFile({
+								file: targetFile,
+								libraryID: parentItem.libraryID,
+								parentItemID: parentItem.id,
+							});
+
+							// Delete the original file
+							item.eraseTx();
+					});
+				});
+			}
+
+	},
+	
+	log(msg) {
+		Zotero.debug("ICloud Attacher: " + msg);
+	},
+
+	addToAllWindows() {
+		var windows = Zotero.getMainWindows();
+		for (let win of windows) {
+			if (!win.ZoteroPane) continue;
+			this.addToWindow(win);
+		}
+	},
+	
+	removeFromWindow(window) {
+		var doc = window.document;
+		// Remove all elements added to DOM
+		for (let id of this.addedElementIDs) {
+			doc.getElementById(id)?.remove();
+		}
+		doc.querySelector('[href="icloud-attacher.ftl"]').remove();
+	},
+	
+	removeFromAllWindows() {
+		var windows = Zotero.getMainWindows();
+		for (let win of windows) {
+			if (!win.ZoteroPane) continue;
+			this.removeFromWindow(win);
+		}
+	},
+
+};
