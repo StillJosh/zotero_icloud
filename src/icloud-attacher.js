045db144d69ac9cb0242cbd4a587686b4d9f2d22
Joshua Stiller <joshua.stiller@tum.de>
2024-02-14 07:23:26 +0100

    ENH: Added a button "update from Zotero" in the view pane, which updates the icloud tags of the  current in-view item

--- src/icloud-attacher.js
+++ src/icloud-attacher.js
@@ -176,5 +176,55 @@ ICloudAttacher = {
 			this.removeFromWindow(win);
 		}
 	},
+    /**
+     * This function updates the tags of PDF attachments in iCloud according to the Zotero Tags.
+     * It retrieves the active Zotero pane and the iCloud path from the preferences.
+     * It then iterates over all items in the pane, and for each item, it gets its attachments.
+     * For each attachment, if it is a PDF, it constructs the iCloud path for the attachment,
+     * retrieves the tags of the item, and writes these tags to the iCloud file.
+     */
+    updateTagsFromZotero: function () {
+        Zotero.debug("Updating tags from Zotero");
+
+        // Get all items in the active Zotero pane (i.e. the currently open library and collection)
+        const pane = Zotero.getActiveZoteroPane();
+        let items = pane.getSortedItems();
+
+        const iCloudPath = Zotero.Prefs.get('extensions.icloud-attacher.iCloudPath', true);
+        // Iterate over all items in the pane
+        for (let item in items) {
+
+            // Get all attachments of the item and iterate over them
+            let attachments = items[item].getAttachments();
+            for (let a in attachments) {
+                let attachment = Zotero.Items.get(attachments[a]);
+
+                // If the attachment is a linked PDF, get its full path and write the tags to the file
+                if (attachment.attachmentContentType === 'application/pdf') {
+                    var it = OS.Path.join(iCloudPath,
+                                            attachment.attachmentPath.replace(/^.*attachments:PaperLibrary\//, ''));
+                    var tags = items[item].getTags();
+                    tags = Object.values(tags).map(item => item.tag);
+                    Zotero.icloudAttacher.writeTags(it, tags);
+                }
+            }
+        }
+    },
+        let doc = window.document;
+
+        // Use Fluent for localization
+        window.MozXULElement.insertFTLIfNeeded("icloud-attacher.ftl");
+
+        // Add menu options to view menu for updating icloud tags
+        let menuitemfromZotero = doc.createXULElement('menuitem');
+        menuitemfromZotero.id = 'update-Tags-From-Zotero';
+        menuitemfromZotero.setAttribute('type', 'normal');
+        menuitemfromZotero.setAttribute('data-l10n-id', 'update-Tags-From-Zotero');
+        // MozMenuItem#checked is available in Zotero 7
+        menuitemfromZotero.addEventListener('command', () => {
+            ICloudAttacher.updateTagsFromZotero();
+        });
+        doc.getElementById('menu_viewPopup').appendChild(menuitemfromZotero);
+        this.storeAddedElement(menuitemfromZotero);
 
 };
--- src/locale/en-US/icloud-attacher.ftl
+++ src/locale/en-US/icloud-attacher.ftl
@@ -1,2 +1,4 @@
 make-it-red-green-instead =
     .label = Make It Green Instead
+update-Tags-From-Zotero =
+    .label = Update Zotero Tags
